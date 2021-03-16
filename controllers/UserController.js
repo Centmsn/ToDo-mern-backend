9da8401edd5ff236cb5ad0eb594f8ea3b7ce4269
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2021-03-16 14:10:05 +0100

    create controller for users/signup

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,25 +1,34 @@
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
+
 const User = require("../models/User");
 const Error = require("../models/Error");
 
-const postUserSignup = async (req, res, next) => {
+const postUserLogin = async (req, res, next) => {
   console.log(req.body);
 
   res.json({ message: "user OK" });
 };
 
-const postUserLogin = async (req, res, next) => {
+const postUserSignup = async (req, res, next) => {
+  // express-validator errors
+  const errors = validationResult(req);
   const { name, email, password } = req.body;
 
-  const newUser = new User({
-    name,
-    email,
-    password,
-  });
+  //   if errors is not empty that means invalid data was passed by the user
+  if (!errors.isEmpty()) {
+    const mapped = errors.mapped(error => error.param);
+    if (mapped.password) {
+      return next(new Error("Password must have 6-20 characters", 422));
+    } else if (mapped.name) {
+      return next(new Error("Name is required.", 422));
+    }
+  }
 
   let existingUser;
 
   try {
-    existingUser = await User.find({ email });
+    existingUser = await User.findOne({ email });
   } catch (error) {
     return next(
       new Error("Email already taken, please choose different one.", 422)
@@ -32,13 +41,28 @@ const postUserLogin = async (req, res, next) => {
     });
   }
 
+  let hashedPassword;
+
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (error) {
+    return next(new Error("User not created, please try again later", 500));
+  }
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
   try {
     await newUser.save();
   } catch (error) {
     return next("Something went wrong, please try again later", 500);
   }
 
-  res.json({ message: "OK" });
+  res.status(201);
+  res.json({ message: "success" });
 };
 
 exports.postUserSignup = postUserSignup;
--- routes/User.js
+++ routes/User.js
@@ -1,10 +1,18 @@
 const { Router } = require("express");
+const { check } = require("express-validator/check");
 
 const UserController = require("../controllers/UserController");
 
 const router = Router();
 
-router.post("/signup", UserController.postUserSignup);
+router.post(
+  "/signup",
+  [
+    check("password").isLength({ min: 6, max: 20 }),
+    check("name").not().isEmpty(),
+  ],
+  UserController.postUserSignup
+);
 
 router.post("/login", UserController.postUserLogin);
 
